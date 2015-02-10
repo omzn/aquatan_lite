6e45d078547a04555a01e26a8e7710e7f6c36125
Osamu Mizuno <o.mizuno@ieee.org>
2015-02-10 13:14:06 +0900

    removed vertical move

--- src/motor.c
+++ src/motor.c
@@ -14,11 +14,8 @@
 /* pin 12 */
 #define LIGHT1_SENSOR 1
 
-volatile static int v_pos;
 volatile static int h_pos;
-volatile static int v_stop;
 volatile static int h_stop;
-volatile static int lift_d;
 volatile static int move_d;
 
 void startMotor(int fd,uint8_t accel,int direction) {
@@ -57,22 +54,6 @@ void onHLight(void) {
   --h_pos;
 }
 
-void onVTouch(void) {
-  wiringPiISR(TOUCH2_SENSOR, INT_EDGE_RISING,  &doNothing);
-#ifdef DEBUG
-  fprintf(stderr,"INT V_Touch!\n");
-#endif
-  --v_pos;
-  v_stop = 1;
-}
-
-void onVLight(void) {
-#ifdef DEBUG
-  fprintf(stderr,"INT V_Light!\n");
-#endif
-  --v_pos;
-}
-
 /* main */
 int main(int argc,char *argv[]) {
   uint8_t accel  = 0x3f;
@@ -108,18 +89,6 @@ int main(int argc,char *argv[]) {
       fprintf(stderr,"%c H direction = %d\n",result,h_direction);
 #endif
       break;
-    case 'u': // Up n positions
-      v_direction = (int)strtol(optarg,NULL,0) * UP_DIR;
-#ifdef DEBUG
-      fprintf(stderr,"%c V direction = %d\n",result,v_direction);
-#endif
-      break;
-    case 'd': // Down n positions
-      v_direction = (int)strtol(optarg,NULL,0) * UP_DIR * -1;
-#ifdef DEBUG
-      fprintf(stderr,"%c V direction = %d\n",result,v_direction);
-#endif
-      break;
     case 's': // force stop
       stop = 1;
       break;
@@ -131,39 +100,25 @@ int main(int argc,char *argv[]) {
     return -1;
   }
 
+  /* Should I do anything? */
+  if (h_direction == 0) {
+    printf("H:0\n");
+    return 0;
+  }
+
+  move_d = wiringPiI2CSetup(DRV8830_SLAVE_1) ;
+  if (move_d < 0) {
+    fprintf(stderr,"Cannot open DRV8830_SLAVE_2\n");
+    return -1;
+  }
   if (stop == 1) {
-    stopMotor(lift_d);
     stopMotor(move_d);
     printf("STOP\n");
     return 0;
   }
 
-  /* Should I do anything? */
-  if (h_direction == 0 && v_direction== 0) {
-    printf("H:0\n");
-    printf("V:0\n");
-    return 0;
-  }
-
-  lift_d = wiringPiI2CSetup(DRV8830_SLAVE_1) ;
-  if (lift_d < 0) {
-    fprintf(stderr,"Cannot open DRV8830_SLAVE_1\n");
-    return -1;
-  }
-
-  move_d = wiringPiI2CSetup(DRV8830_SLAVE_2) ;
-  if (move_d < 0) {
-    fprintf(stderr,"Cannot open DRV8830_SLAVE_2\n");
-    return -1;
-  }
-
   h_pos = abs(h_direction);
-  v_pos = abs(v_direction);
   h_stop = h_pos == 0 ? 2 : 0;
-  v_stop = v_pos == 0 ? 2 : 0;
-
-  pinMode(MOVE_LED,OUTPUT);
-  digitalWrite(MOVE_LED,1);
 
   if (h_direction != 0) {
     /* set interrupt for sensors */
@@ -184,23 +139,8 @@ int main(int argc,char *argv[]) {
     startMotor(move_d,accel,h_direction * LEFT_DIR);
   }
 
-  if (v_direction != 0) {
-    /* set interrupt for sensors */
-    wiringPiISR(LIGHT2_SENSOR, INT_EDGE_FALLING, &onVLight);
-    wiringPiISR(TOUCH2_SENSOR, INT_EDGE_RISING,  &onVTouch);
-
-    /* clear fault regisiter */
-    wiringPiI2CWriteReg8(lift_d,0x01,0x00);
-    /* float */
-    wiringPiI2CWriteReg8(lift_d,0x00,0x00);
-    /* break */
-    wiringPiI2CWriteReg8(lift_d,0x00,0x03);
-    // start motor 
-    startMotor(lift_d,accel,v_direction * UP_DIR);
-  }
-
   // loop 
-  while(h_stop < 2 || v_stop < 2) {
+  while(h_stop < 2 ) {
     if (h_stop == 1) {
 #ifdef DEBUG
       fprintf(stderr,"A h_stop = %d\n",h_stop);
@@ -215,40 +155,19 @@ int main(int argc,char *argv[]) {
       stopMotor(move_d);
       h_stop = 2;
     }
-    if (v_stop == 1) {
-#ifdef DEBUG
-      fprintf(stderr,"C v_stop = %d\n",v_stop);
-#endif
-      stopMotor(lift_d);
-      v_stop = 3;
-    }
-    if (v_pos == 0 && v_stop == 0) {
-#ifdef DEBUG
-      fprintf(stderr,"D v_stop = %d\n",v_stop);
-#endif
-      stopMotor(lift_d);
-      v_stop = 2;
-    }
   } 
   
 #ifdef DEBUG
   fprintf(stderr,"h_stop = %d\n",h_stop);
 #endif
 
-  printf("%d %d\n",(abs(h_direction)-h_pos)*(h_direction>0?1:-1),(abs(v_direction)-v_pos)*(v_direction>0?1:-1));
+  printf("%d\n",(abs(h_direction)-h_pos)*(h_direction>0?1:-1));
 
   if (h_stop == 3) {
     startMotor(move_d,accel,-h_direction * LEFT_DIR);
     delay(600);
     stopMotor(move_d);
   }
-  if (v_stop == 3) {
-    startMotor(lift_d,accel,-v_direction * UP_DIR);
-    delay(600);
-    stopMotor(lift_d);
-  }
-
-  digitalWrite(MOVE_LED,0);
 
   return 0;
 }
