bf70dd71f74c6b6f259a9eba5e3b82ec3cfed13e
Osamu Mizuno <o.mizuno@ieee.org>
2015-02-10 15:53:18 +0900

    setup.sh changed

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo apt-get install libconfig-pit-perl libencode-perl liblwp-useragent-perl libmoose-perl libnet-twitter-perl libsub-exporter-perl 
+sudo apt-get install libconfig-pit-perl libencode-perl libmoose-perl libnet-twitter-perl libsub-exporter-perl 
