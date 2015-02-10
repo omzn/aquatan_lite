bd76430e050922b3983c0f65dfe40dcb3498a05c
Osamu Mizuno <o-mizuno@kit.ac.jp>
2015-02-10 19:20:52 +0900

    add packages

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo apt-get install libconfig-pit-perl libencode-perl libmoose-perl libnet-twitter-perl libsub-exporter-perl 
+sudo apt-get install libconfig-pit-perl libencode-perl libmoose-perl libnet-twitter-perl libsub-exporter-perl libanyevent-perl libanyevent-http-perl libyaml-perl libcrypt-ssleay-perl
