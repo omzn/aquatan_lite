8b772fdb553010fb42eeb38e5ac03c1035e080ea
Osamu Mizuno <o-mizuno@kit.ac.jp>
2015-02-12 18:53:13 +0900

    package script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-
-sudo apt-get install libconfig-pit-perl libencode-perl libmoose-perl libnet-twitter-perl libsub-exporter-perl libanyevent-perl libanyevent-http-perl libyaml-perl libcrypt-ssleay-perl libio-socket-ssl-perl libcarp-clan-perl libtest-warn-perl
-sudo apt-get remove libnet-twitter-perl
-echo It will take much time to install Net::Twitter...
+sudo apt-get -y install libconfig-pit-perl libencode-perl libmoose-perl libnet-twitter-perl libsub-exporter-perl libanyevent-perl libanyevent-http-perl libyaml-perl libcrypt-ssleay-perl libio-socket-ssl-perl libcarp-clan-perl libtest-warn-perl
+sudo apt-get -y remove libnet-twitter-perl
 sudo LANG=C cpan install Net::Twitter
